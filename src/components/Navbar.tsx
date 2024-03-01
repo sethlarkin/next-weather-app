5ebc276e694c3fd9fa51d62acf6ad3b5d6cb88f9
seth-consulting <sethlarkin@icloud.com>
2024-03-01 13:18:32 -0800

    Navbar finished

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { MdWbSunny, MdMyLocation, MdOutlineLocationOn } from "react-icons/md";
+import Searchbox from "./Searchbox";
+
+export default function Navbar() {
+  return (
+    <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
+      <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
+        <p className="flex items-center justify-center gap-2">
+          <h2 className="text-gray-500 text-3xl">Weather</h2>
+          <MdWbSunny className="text-3xl mt-1 text-yellow-300" />
+        </p>
+        <section className="flex gap-2 items-center">
+          <MdMyLocation className="text-3xl text-gray-400 hover:opacity-80 cursor-pointer" />
+          <MdOutlineLocationOn className="text-3xl " />
+          <p className="text-slate-900/80 text-sm">Capitola, CA</p>
+          <div>
+          <Searchbox />
+          </div>
+        </section>
+      </div>
+    </nav>
+  );
+}
